8ba5af2db159138e5a3148a62c83a785abb4932d
M-Sandberg <56336779+SandbergM@users.noreply.github.com>
2024-01-11 15:04:03 +0100

    no msg

--- exchange_rates_gaps/main.py
+++ exchange_rates_gaps/main.py
@@ -1,13 +1,8 @@
-import pandas as pd 
 
 
-"""
+import pandas as pd
 
 
-
-"""
-
-
-df = pd.read_csv( './exchange_rates_gaps/riksbanken_exchange_rates.csv' )
-
-print( df[df["currency"] == "USD"] )
+# DataFrames to use
+orders_df = pd.read_csv( './exchange_rates_gaps/data/orders_data.csv' )
+exchange_rates_df = pd.read_csv( './exchange_rates_gaps/data/riksbanken_exchange_rates.csv' )
